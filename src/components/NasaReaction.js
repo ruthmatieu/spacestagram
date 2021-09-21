f56568ff21602f30b8a74e870dee465738d7dd99
matieuruth <matieu.ruth@gmail.com>
2021-09-21 12:41:40 -0400

    added like comp

--- src/components/NasaReaction.js
+++ src/components/NasaReaction.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiFillLike } from 'react-icons/ai';
+import { AiFillDislike } from 'react-icons/ai';
 
 const NasaReaction = () => {
+    const [count, setCount] = useState(0);
+
+    const likeImage = () => {
+        setCount(count + 1)
+    }
+
+    const dislikeImage = () => {
+        setCount(count - 1)
+    }
+
     return (
         <div>
-            like/dislike
+            {count <= 0 ? <p>Be the first to like this post</p>
+            : count === 1 ? <p>{count} like</p>
+            : <p>{count} likes</p>
+            }
+            <button><AiFillLike onClick={likeImage} style={{color: 'green'}}/></button>
+            <button><AiFillDislike onClick={dislikeImage} style={{color: 'red'}}/></button>
         </div>
     )
 }
